2bd7bb798e2c999e8e68a759e3723693f3001be8
mim-mel <mfblade2014@gmail.com>
2022-09-24 10:50:26 +0900

    feat: Search 페이지에서 검색창에 검색어 입력시 데이터를 불러오는 기능 구현

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,4 +1,35 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "../../api/axios";
+
 const SearchPage = () => {
+  const [searchResults, serSearchResults] = useState([]);
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchResults) => {
+    try {
+      const request = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      console.log(request);
+      serSearchResults(request.data.results);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
   return <div></div>;
 };
 
