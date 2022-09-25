dc59f78a12367ef98017a76b1c786ae9001c6573
mim-mel <mfblade2014@gmail.com>
2022-09-25 10:03:24 +0900

    feat: search 페이지에서 불러온 데이터를 활용해 UI 구현

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import styled from "styled-components";
 import axios from "../../api/axios";
 
 const SearchPage = () => {
@@ -30,7 +31,72 @@ const SearchPage = () => {
     }
   };
 
-  return <div></div>;
+  const renderSearchResults = () => {
+    return searchResults.length > 0 ? (
+      <SearchContainer>
+        {searchResults.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== "person") {
+            const movieImageUrl =
+              "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+            return (
+              <Movie>
+                <MovieColumnPoster>
+                  <MoviePoster src={movieImageUrl} alt="movie" />
+                </MovieColumnPoster>
+              </Movie>
+            );
+          }
+        })}
+      </SearchContainer>
+    ) : (
+      <NoResultsContainer>
+        <NoResultsText>
+          <p>찾고자 하는 검색어 "{searchTerm}"에 맞는 영화가 없습니다.</p>
+        </NoResultsText>
+      </NoResultsContainer>
+    );
+  };
+
+  return renderSearchResults();
 };
+
+const SearchContainer = styled.section`
+  background-color: black;
+  width: 100%;
+  text-align: center;
+  padding: 5rem 0;
+`;
+
+const Movie = styled.div`
+  flex: 1 1 auto;
+  display: inline-block;
+  padding-right: 0.5rem;
+  padding-bottom: 7rem;
+`;
+
+const MovieColumnPoster = styled.div`
+  cursor: pointer;
+  transition: transform 0.3s;
+  -webkit-transition: transform 0.3s;
+  :hover {
+    transform: scale(1.25);
+  }
+`;
+
+const MoviePoster = styled.img`
+  width: 90%;
+  border-radius: 5px;
+`;
+
+const NoResultsContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  color: #c5c5c5;
+  height: 100%;
+  padding: 8rem;
+`;
+
+const NoResultsText = styled.div``;
 
 export default SearchPage;
