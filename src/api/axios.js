01bd8d668cba5653d737d2c3f813403f3e9f7c24
mim-mel <mfblade2014@gmail.com>
2022-09-30 16:59:01 +0900

    feat: axios파일의 api key를 env 파일안에 넣음

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
+  baseURL: 'https://api.themoviedb.org/3',
   params: {
-    api_key: "d6a23af73340e83ad2a7d2ac491aab52",
-    language: "ko-KR",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
+    language: 'ko-KR',
   },
 });
 
